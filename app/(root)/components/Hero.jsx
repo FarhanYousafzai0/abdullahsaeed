84c77c948f295130ddb223bec2e48eb4b654d3c0
Farhan Yousafzai <coderwithferry@gmail.com>
2025-07-11 00:45:07 +0500

    Done Project

--- app/(root)/components/Hero.jsx
+++ app/(root)/components/Hero.jsx
@@ -97,7 +97,7 @@ const Hero = () => {
             <div className="hidden lg:flex">
             <div className="relative inline-flex w-fit group">
               <div className="absolute transition-all duration-1000  opacity-70 -inset-px bg-gradient-to-r from-[#44BCFF] via-[#FF44EC] to-[#FF675E] rounded-xl blur-lg group-hover:opacity-100 group-hover:-inset-1 group-hover:duration-200 animate-tilt" />
-              <button type="submit"  className="relative inline-flex cursor-pointer  items-center justify-center h-[3rem] w-[8rem] text-lg  text-white transition-all duration-200 bg-gray-900 rounded-xl">
+              <button type="submit"  className="relative inline-flex cursor-pointer p-2 items-center justify-center h-[3rem] w-[8rem] text-lg  text-white transition-all duration-200 bg-gray-900 rounded-xl">
                 Join community
               </button> 
             </div>
