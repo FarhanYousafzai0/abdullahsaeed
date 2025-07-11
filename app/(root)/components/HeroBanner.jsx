e55344aa4d749e1ac58b9d9568ab19fd619a3d7c
Farhan Yousafzai <coderwithferry@gmail.com>
2025-07-11 18:06:04 +0500

    Done

--- app/(root)/components/HeroBanner.jsx
+++ app/(root)/components/HeroBanner.jsx
@@ -39,7 +39,7 @@ const HeroBanner = () => {
               <div className="relative inline-flex group">
                 <div className="absolute transition-all duration-1000 opacity-70 -inset-px bg-gradient-to-r from-[#44BCFF] via-[#FF44EC] to-[#FF675E] rounded-xl blur-lg group-hover:opacity-100 group-hover:-inset-1 group-hover:duration-200 animate-tilt" />
                 <Link href="/contact">
-                  <button className="relative cursor-pointer inline-flex items-center justify-center px-8 py-4 text-lg font-semibold text-white bg-gray-900 rounded-xl">
+                  <button className="relative cursor-pointer inline-flex items-center justify-center px-8 py-4 text-lg font-semibold text-white bg-[#0077B5] rounded-xl">
                     Get Started
                   </button>
                 </Link>
